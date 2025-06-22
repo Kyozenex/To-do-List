76ccf206d2482f2d4c2b7f90c24922a8f318b1a7
Kyozenex <kirillzujkov131@gmail.com>
2025-06-22 19:39:18 +0300

    Create To-do list.py

---
+++ To-do list.py
@@ -0,0 +1,47 @@
+tasks = []
+
+while True:
+    print("=== To-Do List ===")
+    print("Нажми 1 чтобы посмотреть список задач")
+    print("Нажми 2 чтобы добавить задачу")
+    print("Нажми 3 чтобы удалить задачу")
+    print("Нажми 4 чтобы выйти")
+
+    try:
+        a = int(input("Введите номер действия: "))
+        if a == 1:
+            if not tasks:
+                print("Нет задач")
+            else:
+                print("Твои задачи:")
+                for index, task in enumerate(tasks):
+                    print(index + 1, task)
+        elif a == 2:
+            task = input("Введите задачу: ")
+            if task.strip():
+                tasks.append(task)
+                print("Твои задачи:", tasks)
+            else:
+                print("Ошибка: задача не может быть пустой!")
+        elif a == 3:
+            if not tasks:
+                print("Удалять нечего")
+            else:
+                print(tasks)
+                print("Какую задачу вы хотите удалить?")
+                try:
+                    number = int(input("Введите номер задачи: "))
+                    if 1 <= number <= len(tasks):
+                        del tasks[number - 1]
+                        print("Твои задачи:", tasks)
+                    else:
+                        print("Ошибка: такого номера задачи нет!")
+                except ValueError:
+                    print("Ошибка: введи число!")
+        elif a == 4:
+            print("Пока!")
+            break  
+        else:
+            print("Ошибка: выбери от 1 до 4!")
+    except ValueError:
+        print("Ошибка: введи число!")
